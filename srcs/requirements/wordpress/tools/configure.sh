13f774a59e94dd270af9efb31fc145bb1443a7da
Francesco Drudi <fdrudi@e3r10p2.42roma.it>
2023-01-16 17:38:25 +0100

    ftp-server && site

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -12,6 +12,7 @@ then
 
 	mv /tmp/index.html /var/www/html/wordpress/index.html
 
+	# Enable FTP Access plugin
 
 	wp core download --allow-root
 	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --dbhost=$MYSQL_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
@@ -19,6 +20,8 @@ then
 	wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
 	wp theme activate twentytwentythree --allow-root
+
+	wp plugin install ftp-access --activate --allow-root
 
 	# Enable Redis Object Cache plugin
 	wp plugin install redis-cache --activate --allow-root
@@ -28,7 +31,7 @@ then
 	wp config set object_cache_redis_port $REDIS_PORT --allow-root
 
 	wp plugin update --all --allow-root
-	
+
 	#  # enable redis cache
 	# sed -i "40i define( 'WP_REDIS_HOST', '$REDIS_HOSTNAME' );"  wp-config.php
 	# sed -i "41i define( 'WP_REDIS_PORT', '$REDIS_PORT' );"      wp-config.php
