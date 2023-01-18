f62a3b636215e5bf5c5b2e2eb8917b920fc441ac
Francesco Drudi <fdrudi@e3r7p1.42roma.it>
2023-01-18 18:13:29 +0100

    FINISHED

--- srcs/requirements/tools/configure.sh
+++ srcs/requirements/tools/configure.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "/home/fdrudi/data" ];
 then
-	mkdir -p /home/fdrudi/data/wp_files /home/fdrudi/data/wp_db
+	mkdir -p /home/fdrudi/data/wp_files /home/fdrudi/data/wp_db /home/fdrudi/data/portainer_data
 fi
 
 if [ `cat /etc/hosts | grep fdrudi.42.fr | wc -l` -eq 0 ];
